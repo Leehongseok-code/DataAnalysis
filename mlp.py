2d885c96435d7004597c1809304b2e40c7a40dfd
Leehongseok-code <52267586+Leehongseok-code@users.noreply.github.com>
2023-12-25 17:10:12 +0900

    first commit

---
+++ mlp.py
@@ -0,0 +1,22 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+# 모델 파라미터 설정
+input_shape = 12  # 예시로 설정한 입력 특성의 수
+num_classes = 4    # 출력 클래스의 수
+
+# MLP 모델 구성
+model = Sequential([
+    Dense(128, activation='relu', input_shape=(input_shape,)),
+    Dense(64, activation='relu'),
+    Dense(1, activation='linear')
+])
+
+# 모델 컴파일
+model.compile(optimizer=Adam(learning_rate=0.001),
+              loss='mse',
+              metrics=['accuracy'])
+
+# 모델 요약
+model.summary()
